2038e52854a29e8fe13a6b18d00ccc952019c056
Axel <axel.mesnard@kedgebs.com>
2021-05-21 16:39:34 +0200

    method to run the app

--- grandpy/app.py
+++ grandpy/app.py
@@ -1,17 +1,38 @@
+from grandpy.parser import Parser
+from grandpy.geocoding import GeocodingClient
+from grandpy.wikipedia import WikipediaClient
+from pprint import pprint
 
-def answer(question):
-    # 1. Question nettoyée = utiliser une classe Parser pour
-    #    analyser la question et conserver uniquement l'info
-    #    utile (c'est à dire l'info de lieu)
 
-    # 2. La question nettoyée est envoyée à une API de geocoding
-    #    (here.com) pour obtenir (a) l'adresse du lieu et (b)
-    #    les coordonnées de latitude et longitude
+class App:
 
-    # 3. Envoyer la latitude et la longitude à l'API wikipedia
-    #    qui va nous envoyer des articles liés à ces coordonnées.
-    #    On choisit l'article consernant le lieu le plus proche
+    def answer(self, question):
+        # 1. Question nettoyée = utiliser une classe Parser pour
+        #    analyser la question et conserver uniquement l'info
+        #    utile (c'est à dire l'info de lieu)
+        parser = Parser()
+        keywords = parser.execute_parser(question)
 
-    return {
-        # Infos récupérées en réponse à la question
-    }
+        # 2. La question nettoyée est envoyée à une API de geocoding
+        #    (here.com) pour obtenir (a) l'adresse du lieu et (b)
+        #    les coordonnées de latitude et longitude
+        geocoding_client = GeocodingClient()
+        geocoding_client_data = geocoding_client.search(keywords)
+        latitude = geocoding_client_data['lat']
+        longitude = geocoding_client_data['lng']
+        
+        # return latitude
+
+        # 3. Envoyer la latitude et la longitude à l'API wikipedia
+        #    qui va nous envoyer des articles liés à ces coordonnées.
+        #    On choisit l'article consernant le lieu le plus proche
+        wikipedia_client = WikipediaClient()
+        wikipedia_client_data = wikipedia_client.search(latitude, longitude)
+
+        return wikipedia_client_data
+            # Infos récupérées en réponse à la question
+        
+
+app = App()
+answer = app.answer("Bonjour ou se trouve la mairie de la ciotat ?")
+pprint(answer)
