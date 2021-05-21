790dece6d5a3122806d735332f1b81c74a601216
Axel <axel.mesnard@kedgebs.com>
2021-05-21 15:48:35 +0200

    small tests update

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,7 @@
 from grandpy.parser import Parser
 
 class TestParser:
+
     def test_remove_all_accents_from_sentence(self):
         parser = Parser()
         result = parser.remove_all_accents("où é")
@@ -24,21 +25,21 @@ class TestParser:
     def test_extract_place_ou_est(self):
         parser = Parser()
         result = parser.extract_place("Bonjour ou est Notre Dame de Paris ?")
-        assert result == "Notre Dame de Paris"
+        assert result == "Notre Dame de Paris "
 
     def test_extract_place_ou_se_trouve(self):
         parser = Parser()
         result = parser.extract_place("Bonjour ou se trouve la mairie de la ciotat ?")
-        assert result == "la mairie de la ciotat"
+        assert result == "la mairie de la ciotat "
 
     def test_extract_place_ou_se_situe(self):
         parser = Parser()
         result = parser.extract_place("Bonjour ou se situe le port du havre ?")
-        assert result == "le port du havre"
+        assert result == "le port du havre "
 
     def test_parser_execute(self):
         parser = Parser()
         sentence = "Bonjour, où se situe Notre-Dames-des-L'andes ?"
         result  = parser.execute_parser(sentence)
-        assert result == "notre dames des landes"
+        assert result == "notre dames des landes "
 
