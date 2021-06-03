5923a5f127af7bbdb3aa24488e559952e59cfd2a
Axel <axel.mesnard@kedgebs.com>
2021-06-03 15:40:58 +0200

    JS update

--- website/static/main.js
+++ website/static/main.js
@@ -1,3 +1,4 @@
+
 let form = document.querySelector("#user-text-form");
 
 function postFormData(url, data, headers) {
@@ -10,6 +11,45 @@ function postFormData(url, data, headers) {
     .catch(error => console.log(error));
 }
 
+
+
+function buildResponse(response) {
+    const answer = document.createElement("div")
+    answer.classList.add("answer")
+
+    const answerGranpyAddress = document.createElement("p");
+    answerGranpyAddress.classList.add("answer-grandpy-address");
+    answerGranpyAddress.textContent = response.grandpy_address
+
+    answer.appendChild(answerGranpyAddress);
+
+    const mapDiv = document.createElement("div")
+    mapDiv.classList.add("map")
+    mapDiv.setAttribute("id", "map");
+
+    let map;
+
+    function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+    }
+
+    var script = document.createElement('script');
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDv4_XB-nvhxiZU-vh70nohrUKqZLaQSPU&callback=initMap';
+    script.async = true;
+
+    window.initMap = function() {};
+
+    mapDiv.appendChild(script);
+
+    answer.appendChild(mapDiv);
+
+    const conversationContainer = document.querySelector("#conversationContainer");
+    conversationContainer.appendChild(answer);
+}
+
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     
@@ -17,7 +57,7 @@ form.addEventListener("submit", function(event) {
         "Content-type": "plain/text"
     })
     .then(response => {
-        console.log(response);
+        buildResponse(response);
     })
 
 })
