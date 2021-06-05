6070061af16982b43c81f7b27d402e58bb25a830
Axel <axel.mesnard@kedgebs.com>
2021-06-05 15:20:00 +0200

    small naming update

--- website/views.py
+++ website/views.py
@@ -8,7 +8,7 @@ def homepage():
     return render_template("index.html")
 
 @app.route("/grandpy", methods=["POST"])
-def server():
+def grandpy():
     user_text = request.data.decode()
     parser = App()
     response = parser.answer(user_text)
