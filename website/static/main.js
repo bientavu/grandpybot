9ac3bfd3b4798828c8e67b7ae601f7f42e4eafa0
Axel <axel.mesnard@kedgebs.com>
2021-06-05 15:19:21 +0200

    severals update + new function

--- website/static/main.js
+++ website/static/main.js
@@ -11,53 +11,96 @@ function postFormData(url, data, headers) {
     .catch(error => console.log(error));
 }
 
-
-
-function buildResponse(response) {
-    const answer = document.createElement("div")
-    answer.classList.add("answer")
-
-    const answerGranpyAddress = document.createElement("p");
-    answerGranpyAddress.classList.add("answer-grandpy-address");
-    answerGranpyAddress.textContent = response.grandpy_address
-
-    answer.appendChild(answerGranpyAddress);
-
-    const mapDiv = document.createElement("div")
-    mapDiv.classList.add("map")
-    mapDiv.setAttribute("id", "map");
-
-    let map;
-
-    function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-    });
-    }
-
-    var script = document.createElement('script');
-    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDv4_XB-nvhxiZU-vh70nohrUKqZLaQSPU&callback=initMap';
-    script.async = true;
-
-    window.initMap = function() {};
-
-    mapDiv.appendChild(script);
-
-    answer.appendChild(mapDiv);
+function buildQuestion(value) {
+    const question = document.createElement("div")
+    question.classList.add("question")
+    question.textContent = value
 
     const conversationContainer = document.querySelector("#conversationContainer");
-    conversationContainer.appendChild(answer);
+    conversationContainer.appendChild(question);
+
 }
+
+function initMap(mapDiv, latitude, longitude) {
+
+    const place = {
+        lat: latitude,
+        lng: longitude
+    };
+    const map = new google.maps.Map(mapDiv, {
+        zoom: 12,
+        center: place,
+    });
+    const marker = new google.maps.Marker({
+        position: place,
+        map: map
+    });
+}
+
+function buildResponse(response) {
+    const answerHeader = document.createElement("div");
+    answerHeader.classList.add("answer");
+    answerHeader.setAttribute("id", "answer");
+    answerHeader.textContent = response.grandpy_address + response.address;
+
+    const answerMap = document.createElement("div");
+    answerMap.classList.add("answer");
+    answerMap.setAttribute("id", "answer-map");
+
+    const answerWiki = document.createElement("div");
+    answerWiki.classList.add("answer");
+    answerWiki.setAttribute("id", "answer-wiki");
+    answerWiki.textContent = response.grandpy_wiki;
+
+    const answerWiki2 = document.createElement("div");
+    answerWiki2.classList.add("answer");
+    answerWiki2.setAttribute("id", "answer-wiki-2");
+    answerWiki2.textContent = response.story;
+    
+    const seeFullStory = document.createElement("div");
+    seeFullStory.classList.add("answer");
+    seeFullStory.setAttribute("id", "seeFullStory");
+    const span = document.createElement("span");
+    const link = document.createElement("a");
+    link.setAttribute('href', response.fullurl);
+    link.setAttribute('target','_blank');
+    link.textContent = "[Voir l'anecdote complète]";
+    span.appendChild(link);
+    seeFullStory.appendChild(link);
+    
+    const conversationContainer = document.querySelector("#conversationContainer");
+    conversationContainer.appendChild(answerHeader);
+    conversationContainer.appendChild(answerMap);
+    initMap(answerMap, response.latitude, response.longitude)
+    conversationContainer.appendChild(answerWiki);
+    conversationContainer.appendChild(answerWiki2);
+    conversationContainer.appendChild(seeFullStory);
+}
+
+function resetInputForm(userText) {
+    userText.reset();
+};
+
+function runLoadAnimation() {
+    let inputButton = document.querySelector('#inputButton');
+    inputButton.classList.add('spinning');
+    setTimeout(() => inputButton.classList.remove('spinning'), 1000);
+  };
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
+
+    runLoadAnimation();
+
+    buildQuestion(document.querySelector("#userText").value)
     
     postFormData("/grandpy", document.querySelector("#userText").value, {
         "Content-type": "plain/text"
-    })
-    .then(response => {
-        buildResponse(response);
-    })
+        })
+        .then(response => {
+            buildResponse(response)
+        })
+        .catch(error => console.log(error));
 
+    resetInputForm(document.querySelector("#userText").value);
 })
