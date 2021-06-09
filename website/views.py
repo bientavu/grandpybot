f0fd3d858e9d4f1d3fedd53a93930134be4883ae
Axel <axel.mesnard@kedgebs.com>
2021-06-09 10:53:05 +0200

    minor changes

--- website/views.py
+++ website/views.py
@@ -1,3 +1,4 @@
+import os
 from werkzeug.wrappers import response
 from website import app
 from flask import render_template, url_for, request, jsonify
@@ -5,7 +6,7 @@ from grandpy.app import App
 
 @app.route("/")
 def homepage():
-    return render_template("index.html")
+    return render_template("index.html", google_key=os.getenv("GOOGLE_FRONT_API_KEY"))
 
 @app.route("/grandpy", methods=["POST"])
 def grandpy():
